d9faf946a33fd9b6ecad67379b6c69ae1a96cdb8
Alexander Bareyko <militaryboy@yandex.ru>
2019-03-17 19:22:25 +0100

    Night mode button

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const container = createElement('div');
+  container.classList.add('container');
   const charts = document.createElement('div');
   charts.classList.add('charts');
-  document.body.appendChild(charts);
+  document.body.appendChild(container);
+  container.appendChild(charts);
+
+  const nightButton = createElement('button');
+  nightButton.innerText = 'Switch to Night Mode';
+  nightButton.classList.add('night-button');
+  nightButton.addEventListener('click', () => {
+    document.body.classList.toggle('night');
+
+    if (document.body.classList.contains('night')) {
+      nightButton.innerText = 'Switch to Day Mode';
+    } else {
+      nightButton.innerText = 'Switch to Night Mode';
+    }
+  });
+
+  container.appendChild(nightButton);
 
   fetch('chart_data.json')
     .then(data => data.json())
